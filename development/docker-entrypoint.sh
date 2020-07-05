ca769bc08e08d4eb5df57a5faeddb6819a99f7f9
Sanjay <info@sanjaybharti.com>
2020-07-05 08:52:49 +0200

    dockerfile and other requirement file

---
+++ development/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z $APP_NAME ]]; then
+	echo "Variable APP_NAME not set - Please set this using the -e flag"
+    exit 1
+else
+    yes | python manage.py makemigrations --merge
+    yes | python manage.py migrate
+    python manage.py loaddata raw_data.json
+	python manage.py collectstatic --noinput
+  uwsgi --http :8001 \
+   --http-websockets \
+   --processes 2 \
+   --threads 2 \
+   --wsgi-file /app/$APP_NAME/wsgi.py
+fi
